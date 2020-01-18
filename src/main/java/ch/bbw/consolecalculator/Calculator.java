a231e4e7021e60893b2d4ade69d4167c81457d0c
timo <timo.leiser@lernende.bbw.ch>
2020-01-18 19:04:32 +0100

    Prepare test for subtraction

--- src/main/java/ch/bbw/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -14,4 +14,8 @@ public class Calculator {
 	public int summe(int summand1, int summand2) {
 		return summand1 + summand2;
 	}
+
+	public int subtraktion(int minuend , int subtrahend) {
+		return 0;
+	}
 }
