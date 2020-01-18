a5991a98e437332b1ff7b9f68d8f4750f3b1ea5c
timo <timo.leiser@lernende.bbw.ch>
2020-01-18 18:49:29 +0100

    Test Calculator class with unittest.

---
+++ src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -0,0 +1,22 @@
+package ch.bbw.consolecalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test Class for Class Calculator
+ * 
+ * @author Timo Leiser
+ * @date   18.01.2020
+ */
+public class CalculatorTest {
+	Calculator testee;
+
+	@Test
+	public void testSummeZweiPositiveIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.summe(10, 25) == 35);
+	}
+
+}
