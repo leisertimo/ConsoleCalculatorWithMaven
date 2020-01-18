dab1f37f650c32735388cd7dc78faa5a59527689
timo <timo.leiser@lernende.bbw.ch>
2020-01-18 15:56:32 +0100

    Implement Addition Method

---
+++ src/main/java/ch/bbw/consolecalculator/Calculator.java
@@ -0,0 +1,17 @@
+package ch.bbw.consolecalculator;
+
+/**
+ * Calculator 
+ * - kann zwei Zahlen addieren
+ * 
+ * @author Timo Leiser
+ * @date 18.01.2020
+ *
+ */
+
+public class Calculator {
+
+	public int summe(int summand1, int summand2) {
+		return summand1 + summand2;
+	}
+}
